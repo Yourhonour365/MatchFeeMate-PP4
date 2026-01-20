96b07cfe9db3cc96bb3eda6aacb1c33c7b17e968
Jed Chamberlain <jed@commerxia.io>
2026-01-20 18:16:19 +0000

    Register models in Django admin, add demo players and opposition teams

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Club
+from .models import Club, Player, Opposition
 
 admin.site.register(Club)
+admin.site.register(Player)
+admin.site.register(Opposition) 
