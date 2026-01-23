55d2934e7ba75f3ab2148907a2d6ed634a193603
Jed Chamberlain <jed@commerxia.io>
2026-01-23 11:14:51 +0000

    Fix: Auto-link players to users bidirectionally with case-insensitive email matching

--- clubs/signals.py
+++ clubs/signals.py
@@ -5,8 +5,22 @@ from .models import Player
 
 User = get_user_model()
 
+
 @receiver(post_save, sender=User)
+def link_user_to_players(sender, instance, created, **kwargs):
+    """When user signs up, link to ALL matching unlinked Players"""
+    if created and instance.email:
+        Player.objects.filter(
+            email__iexact=instance.email,
+            user__isnull=True
+        ).update(user=instance)
+
+
+@receiver(post_save, sender=Player)
 def link_player_to_user(sender, instance, created, **kwargs):
-    """When a user signs up, link them to any Player with matching email"""
-    if created:
-        Player.objects.filter(email=instance.email, user__isnull=True).update(user=instance)
+    """When Player created, link to existing User if email matches"""
+    if created and instance.email and not instance.user:
+        user = User.objects.filter(email__iexact=instance.email).first()
+        if user:
+            instance.user = user
+            instance.save()
